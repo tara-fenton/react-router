11fb5bc94e6d40e3fe1c68f33214befe540d20f2
Steven Ciasullo <sjciasullo@gmail.com>
2018-05-30 11:41:10 -0400

    display debits

--- src/components/Debits.jsx
+++ src/components/Debits.jsx
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
 
 class Debits extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      debits: []
+    }
+  }
+
+  componentDidMount(){
+    fetch("http://localhost:4000/debits", {
+      method: 'GET'
+    }).then((res)=>res.json())
+    .then((json) => {
+      this.setState({
+        debits: json
+      })
+    }).catch( (err) => console.log(err));
+  }
+
   render(){
+    const debitsComponents = this.state.debits.map((debit) => {
+      return (
+        <div className="debit-single">
+          <p>Description: {debit.description}</p>
+          <p>Amount: ${debit.amount}</p>
+          <p>Date: {debit.date}</p>
+        </div>
+      )
+    })
+
     return(
       <div>
         <h2>Debits: </h2>
+        {debitsComponents}
       </div>
     )
   }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,8 +8,8 @@ class Home extends Component{
       <div>
         <img src="https://letstalkpayments.com/wp-content/uploads/2016/04/Bank.png" alt="bank"/>
         <h1>Bank of React</h1>
-        <Link to="/userProfile">User Profile</Link>
-        <Link to="/debits">Debits</Link>
+        <Link to="/userProfile">User Profile</Link><br/>
+        <Link to="/debits">Debits</Link><br/>
         <AccountBalance accountBalance={this.props.accountBalance}/>
       </div>
     )
