fdf13f82f86dafd862ebcafa159735827cf3b328
Steven Ciasullo <sjciasullo@gmail.com>
2018-05-30 12:41:09 -0400

    handle change and stub handlesubmit for debits form

--- src/components/Debits.jsx
+++ src/components/Debits.jsx
@@ -5,7 +5,25 @@ class Debits extends Component{
   constructor(props){
     super(props);
     this.state={
+      description: "",
+      amount: 0
     }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event){
+    const name = event.target.name
+    const value = event.target.value
+    this.setState({
+      [name]:value,
+    })
+  }
+
+  handleSubmit(event){
+    event.preventDefault();
+    
   }
 
   render(){
@@ -23,6 +41,17 @@ class Debits extends Component{
       <div>
         <AccountBalance accountBalance={this.props.accountBalance}/>
         <h2>Debits: </h2>
+        <form onSubmit={this.handleSubmit}>
+          <input name="description" onChange={this.handleChange} type="text" 
+            placeholder="Description" value={this.state.description}
+          />
+          <br />
+          <input name="amount" onChange={this.handleChange} type="number"  
+            placeholder="Amount" value={this.state.amount}
+          />
+          <br />
+          <input type="submit" value="Add"/>
+        </form>
         {debitsComponents}
       </div>
     )
