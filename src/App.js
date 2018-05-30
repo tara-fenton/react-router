f8e68bef42e1180632da2ea02009b8fd45ddaff1
Steven Ciasullo <sjciasullo@gmail.com>
2018-05-30 10:42:18 -0400

    install react-router-dom and put in app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import {BrowserRouter as Router} from 'react-router-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          Hello World
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
