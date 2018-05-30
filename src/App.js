923d90f0c856c22e7a559e91186c6a8853117b3e
Steven Ciasullo <sjciasullo@gmail.com>
2018-05-30 10:51:37 -0400

    pass account balance prop from app -> home

--- src/App.js
+++ src/App.js
@@ -3,11 +3,18 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from './components/Home';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      accountBalance= 14568.27,
+    }
+  }
+
   render() {
     return (
       <Router>
         <Switch>
-          <Route exact path="/" component={Home} />
+          <Route exact path="/" component={<Home accountBalance={this.state.accountBalance}/>} />
         </Switch>
       </Router>
     );
