3ee17dfb6ffc943c1a7b6df4efb0102c3697726e
Steven Ciasullo <sjciasullo@gmail.com>
2018-05-30 10:47:59 -0400

    fix router import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {BrowserRouter as Router} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from './components/Home';
 
 class App extends Component {
